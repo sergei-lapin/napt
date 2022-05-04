e4cb3a492fff9ad56a0d6bdf51c9b8fd83553e54
Sergey Lapin <slapin@vivid.money>
2022-05-04 12:32:49 +0600

    fix windows issue

---
+++ plugins/gradle/src/main/java/com/slapin/napt/task/CreateNaptTrigger.kt
@@ -0,0 +1,43 @@
+package com.slapin.napt.task
+
+import org.gradle.api.DefaultTask
+import org.gradle.api.file.DirectoryProperty
+import org.gradle.api.file.RegularFile
+import org.gradle.api.provider.Property
+import org.gradle.api.provider.Provider
+import org.gradle.api.tasks.*
+
+abstract class CreateNaptTrigger : DefaultTask() {
+
+    @get:Input abstract val projectName: Property<String>
+
+    @get:InputDirectory
+    @get:PathSensitive(PathSensitivity.RELATIVE)
+    abstract val javaDirectory: DirectoryProperty
+
+    @get:Input @get:Optional abstract val packagePrefix: Property<String>
+
+    @get:OutputFile
+    val output: Provider<RegularFile>
+        get() = javaDirectory.map { directory -> directory.file("NaptTrigger.java") }
+
+    @TaskAction
+    fun run() {
+        val triggerCode =
+            """
+                        package ${getPackageName()};
+                        class NaptTrigger {
+                        }
+                    """.trimIndent()
+        val output = output.get().asFile
+        output.parentFile?.mkdirs()
+        output.createNewFile()
+        output.writeText(triggerCode)
+    }
+
+    private fun getPackageName(): String {
+        val packagePrefix = packagePrefix.orNull?.let { "$it." }.orEmpty()
+        val projectName = projectName.get().replace("-", "_")
+        return "$packagePrefix$projectName"
+    }
+}
